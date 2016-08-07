56c12b3b2cf27f2b0f2e288edad1fd8aaac57092
Master of Celebration <erik.milde@googlemail.com>
2016-08-07 15:40:02 +0200

    added basic construct

--- blindschleiche.py
+++ blindschleiche.py
@@ -1,8 +1,12 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
+class Blindschleiche(object):
+    ''' calss for _TODO_ '''
+	def __init__(self):
+	    self.foo = False
 
 if __name__ == "__main__":
-    pass
+    blindschleiche = Blindschleiche()
+	
 
